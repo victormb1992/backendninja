1bb00b85a446d9c251a35a9c07ebd82605d64b94
victormb1992 <victor.mb1992@gmail.com>
2018-07-09 14:55:23 +0200

    backendninja

---
+++ src/main/java/com/udemy/controller/RestController.java
@@ -0,0 +1,20 @@
+package com.udemy.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.udemy.model.ContactModel;
+
+@org.springframework.web.bind.annotation.RestController
+@RequestMapping("/rest")
+public class RestController {
+	
+	@GetMapping("/checkrest")
+	public ResponseEntity<ContactModel> checkRest(){
+		ContactModel contactModel = new ContactModel(2,"Mikel","Pérez","455486786","Madrid");
+		return new ResponseEntity<ContactModel>(contactModel,HttpStatus.OK);
+	}
+
+}
---
+++ src/main/java/com/udemy/repository/LogRepository.java
@@ -0,0 +1,13 @@
+package com.udemy.repository;
+
+import java.io.Serializable;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.udemy.entity.Log;
+
+@Repository("logRepository")
+public interface LogRepository extends JpaRepository <Log,Serializable> {
+
+}
